a8686d324ee09f4e538afa0ddd23b76eec5347e8
sivagollapalli <siva@joshsoftware.com>
2012-11-30 12:42:59 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Talk::Application.config.secret_token = '1dc5da2a5bcbde63b093bc8793ff80067dbf597d8712b9142827d737c53f3e62ea451b707f81f94dd1f2675fea2d04425d2b11dfc30960df76fb8d1bb16ed825'
