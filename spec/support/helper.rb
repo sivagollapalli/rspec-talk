d10af7186a65f2bc5ce2a4cb085a234ea62dfa04
sivagollapalli <siva@joshsoftware.com>
2012-12-03 22:51:07 +0530

    Updated

---
+++ spec/support/helper.rb
@@ -0,0 +1,4 @@
+def sign_in
+  post user_session_path, :user => {:email => admin.email, 
+    :password => 'test123'} 
+end
--- spec/support/sign_in.rb
+++
@@ -1,6 +0,0 @@
-module AuthenticationHelpers
-  def sign_in
-    post user_session_path, :user => {:email => admin.email, 
-                                      :password => 'test123'} 
-  end
-end
